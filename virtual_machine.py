9b8f1dc59b528c57122c6bebef52cfe0545fa3a5
Dhruv Kumar <darbaga10@gmail.com>
2017-07-18 12:22:12 +0530

    Add run() to the vm, which iterates through all the bytecodes and executes them

--- virtual_machine.py
+++ virtual_machine.py
@@ -1,9 +1,12 @@
 class VirtualMachine:
-    def __init__(self, ram_size=256, stack_size=32):
+    def __init__(self, bytecodes, ram_size=256, stack_size=32, executing=True):
+        self.bytecodes = bytecodes
         self.data = [None]*ram_size
         self.stack = [None]*stack_size
         self.stack_size = stack_size
         self.stack_top = 0
+        self.executing = executing
+        self.pc = 0
 
     def push(self, value):
         """Push something onto the stack."""
@@ -26,4 +29,10 @@ class VirtualMachine:
     def write_memory(self, index, value):
         """Write to memory.  Crash if index is out of bounds."""
         self.data[index] = value
+
+    def run(self):
+        while self.executing:
+            self.bytecodes[self.pc].execute(self)
+            if self.bytecodes[self.pc].autoincrement:
+                self.pc += 1
 
