3c4afe4e3de72b106d8fe10f2e934614a7805b06
Dhruv Kumar <darbaga10@gmail.com>
2017-07-18 19:59:50 +0530

    Simplify stack

--- virtual_machine.py
+++ virtual_machine.py
@@ -1,26 +1,18 @@
 class VirtualMachine:
-    def __init__(self, bytecodes, ram_size=256, stack_size=32, executing=True):
+    def __init__(self, bytecodes, ram_size=256, executing=True):
         self.bytecodes = bytecodes
         self.data = [None]*ram_size
-        self.stack = [None]*stack_size
-        self.stack_size = stack_size
-        self.stack_top = 0
+        self.stack = []
         self.executing = executing
         self.pc = 0
 
     def push(self, value):
         """Push something onto the stack."""
-        if self.stack_top+1 > self.stack_size:
-            raise IndexError
-        self.stack[self.stack_top] = value
-        self.stack_top += 1
+        self.stack += [value]
 
     def pop(self):
         """Pop something from the stack. Crash if empty."""
-        if self.stack_top == 0:
-            raise IndexError
-        self.stack_top -= 1
-        return self.stack.pop(self.stack_top)
+        return self.stack.pop()
 
     def read_memory(self, index):
         """Read from memory, crashing if index is out of bounds."""
