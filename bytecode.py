6bb5faf29d8da2bdb85f3d4b78acea03157db35f
Dhruv Kumar <darbaga10@gmail.com>
2017-07-18 08:35:42 +0530

    Add Push and Pop bytecodes, as well as the arithmetic operators

--- bytecode.py
+++ bytecode.py
@@ -1,3 +1,44 @@
 class BytecodeBase:
+    def __init__(self):
+        # Eventually might want to add subclassed bytecodes here
+        # Though __subclasses__ works quite well
+        pass
+
     def execute(self, machine):
         pass
+
+
+class Push(BytecodeBase):
+    def __init__(self, data):
+        self.data = data
+
+    def execute(self, machine):
+        machine.push(self.data)
+
+class Pop(BytecodeBase):
+    def execute(self, machine):
+        return machine.pop()
+
+class Add(BytecodeBase):
+    def execute(self, machine):
+        a = Pop().execute(machine)
+        b = Pop().execute(machine)
+        machine.push(a+b)
+
+class Sub(BytecodeBase):
+    def execute(self, machine):
+        a = Pop().execute(machine)
+        b = Pop().execute(machine)
+        machine.push(a-b)
+
+class Mul(BytecodeBase):
+    def execute(self, machine):
+        a = Pop().execute(machine)
+        b = Pop().execute(machine)
+        machine.push(a*b)
+
+class Div(BytecodeBase):
+    def execute(self, machine):
+        a = Pop().execute(machine)
+        b = Pop().execute(machine)
+        machine.push(a/b)
